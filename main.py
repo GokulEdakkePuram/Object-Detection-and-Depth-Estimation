a93556db7c3d4d5352b66a9d5f328ce08ae09841
Gokul Edakke Puram <ge-235725@hs-weingarten.de>
2024-05-19 19:31:10 +0200

    Function to plot BB. Plotted GroundTruth

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 import cv2
 
+def plotfig(img, x_main, y_main, w, h, color):
+    plt.figure(figsize=(9,9))
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    for bb in range(len(x_main)):
+        rectangle = patches.Rectangle((x_main[bb],y_main[bb]), w[bb], h[bb], fill=False, edgecolor=color)
+        plt.gca().add_patch(rectangle)
+    plt.axis("off")
+    plt.title(f'Scene ID: {scenes:06d}')
+    plt.show()
+
 root = "ComputerVision/Object-Detection-and-Depth-Estimation/KITTI_Selection"
 
 scene_list = [6037,6042,6048,6054,6059,6067,6097,6098,6121,6130,6206,6211,6227,6253,6291,6310,6312,6315,6329,6374]
@@ -11,6 +21,7 @@ scene_list = [6037,6042,6048,6054,6059,6067,6097,6098,6121,6130,6206,6211,6227,6
 for scenes in scene_list:
     img = cv2.imread(f"{root}/images/{scenes:06d}.png")
     calib = np.loadtxt(f"{root}/calib/{scenes:06d}.txt")
+    labels = np.loadtxt(f"{root}/labels/{scenes:06d}.txt", usecols=(1,2,3,4,5))
     # Load a model
     model = YOLO('yolov8x.pt')  # load an official model
     #model = YOLO('path/to/best.pt')  # load a custom model
@@ -24,14 +35,7 @@ for scenes in scene_list:
     w = bb_result[:,2]
     h = bb_result[:,3]
     
-    plt.figure(figsize=(9,9))
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    for bb in range(len(x_main)):
-        rectangle = patches.Rectangle((x_main[bb],y_main[bb]), w[bb], h[bb], fill=False, edgecolor='r')
-        plt.gca().add_patch(rectangle)
-    plt.axis("off")
-    plt.title(f'Scene ID: {scenes:06d}')
-    plt.show()
+    plotfig(img, x_main, y_main, w, h, 'red')
     
     x_dist = bb_result[:,0]
     y_dist = bb_result[:,1]+bb_result[:,3]/2
@@ -51,3 +55,5 @@ for scenes in scene_list:
         
     print(obj_image)
     print(pred_dist)
+    
+    plotfig(img, labels[:,0],labels[:,1],(labels[:,2]-labels[:,0]),(labels[:,3]-labels[:,1]), 'green')
